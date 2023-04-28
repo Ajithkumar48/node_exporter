fe5edc4fac816259d0c43473da47312293b4d893
Ajith <50458195+Ajithkumar48@users.noreply.github.com>
2023-04-28 18:03:41 +0300

    script without rsa key
    This script takes a fourth argument, SSH_PASSWORD, which is the password for the SSH connection. Instead of using the -i option to specify the private key, it uses sshpass to pass the password as an argument to the ssh and scp commands. The -o StrictHostKeyChecking=no option is still used to disable strict host key checking.

---
+++ script_password.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+WORKSPACE=$1
+SERVER=$2
+SSH_USER=$3
+SSH_PASSWORD=$4
+
+# Set the Source directory with WORKSPACE
+SOURCE_DIR="${WORKSPACE}/node_exporter"
+echo "Source folder: ${SOURCE_DIR}"
+
+# Set the destination directory with SSH_USER
+DEST_DIR="/home/${SSH_USER}"
+echo "Destination folder: ${DEST_DIR}"
+
+echo ""
+echo "------------------------------------------------------------------------"
+echo "[STARTED] node_exporter initiated on server: ${SERVER}"
+echo "------------------------------------------------------------------------"
+echo ""
+
+echo "Copying files to ${SERVER}..."
+echo ""
+sshpass -p "${SSH_PASSWORD}" ssh -o StrictHostKeyChecking=no -p 22 "${SSH_USER}@${SERVER}" "rm -r ${DEST_DIR}/node_exporter" > /dev/null
+sshpass -p "${SSH_PASSWORD}" scp -o StrictHostKeyChecking=no -P 22 -r "${SOURCE_DIR}"* "${SSH_USER}@${SERVER}:${DEST_DIR}"
+sshpass -p "${SSH_PASSWORD}" ssh -o StrictHostKeyChecking=no -p 22 "${SSH_USER}@${SERVER}" "cd ${DEST_DIR}/node_exporter && chmod +x ./node_exporter && ./node_exporter" > /dev/null
