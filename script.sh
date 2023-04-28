db837cb3df8c35304bdbc959b90dba69b494f32f
Ajith <50458195+Ajithkumar48@users.noreply.github.com>
2023-04-28 17:55:00 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -20,6 +20,6 @@ echo ""
 
   echo "Copying files to ${SERVER}..."
   echo ""
-  ssh -i /home/vagrant/.ssh/id_rsa -o StrictHostKeyChecking=no -p 22 "${SSH_USER}@${SERVER}" "rm -r ${DEST_DIR}/node_exporter" > /dev/null
-  scp -i /home/vagrant/.ssh/id_rsa -o StrictHostKeyChecking=no -P 22 -r "${SOURCE_DIR}"* "${SSH_USER}@${SERVER}:${DEST_DIR}"
-  ssh -i /home/vagrant/.ssh/id_rsa -o StrictHostKeyChecking=no -p 22 "${SSH_USER}@${SERVER}" "cd ${DEST_DIR}/node_exporter && chmod +x ./node_exporter && ./node_exporter" > /dev/null
+  ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -p 22 "${SSH_USER}@${SERVER}" "rm -r ${DEST_DIR}/node_exporter" > /dev/null
+  scp -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -P 22 -r "${SOURCE_DIR}"* "${SSH_USER}@${SERVER}:${DEST_DIR}"
+  ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -p 22 "${SSH_USER}@${SERVER}" "cd ${DEST_DIR}/node_exporter && chmod +x ./node_exporter && ./node_exporter" > /dev/null
